2e58a7f66da51d2cbd398febf36087b35310e180
hayashidakazumi <axhfaro@gmail.com>
2015-10-21 21:39:17 +0900

    create project

---
+++ SwiftGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGame
+//
+//  Created by HayashidaKazumi on 2015/10/21.
+//  Copyright © 2015年 HayashidaKazumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
