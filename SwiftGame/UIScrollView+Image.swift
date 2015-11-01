5637dcf788c0930644026be34ac96b8bcc89a61b
hayashidakazumi <axhfaro@gmail.com>
2015-11-01 15:14:55 +0900

    Add UIScrollView+Image

---
+++ SwiftGame/UIScrollView+Image.swift
@@ -0,0 +1,9 @@
+//
+//  UIScrollView+Image.swift
+//  SwiftGame
+//
+//  Created by HayashidaKazumi on 2015/11/01.
+//  Copyright © 2015年 HayashidaKazumi. All rights reserved.
+//
+
+import Foundation
