03597fde484828c9452650accdecbff474c163fc
hayashidakazumi <axhfaro@gmail.com>
2015-11-05 22:55:46 +0900

    Fix ScrollView

--- SwiftGame/ViewController.swift
+++ SwiftGame/ViewController.swift
@@ -9,39 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var imageView: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-//        view.frame = CGRectMake(0, 0, CGRectGetWidth(UIScreen.mainScreen().bounds), CGRectGetWidth(UIScreen.mainScreen().bounds))
-        
-        print("window \(UIScreen.mainScreen().bounds.size)")
-        print("viewDidLoad \(scrollView.frame)")
-    }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        print("will apper \(scrollView.frame)")
-
-    }
+    //        view.frame = CGRectMake(0, 0, CGRectGetWidth(UIScreen.mainScreen().bounds), CGRectGetWidth(UIScreen.mainScreen().bounds))
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        print("will apper \(scrollView.frame)")
-        
-        scrollView.fixImage(imageView: imageView)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    print("window \(UIScreen.mainScreen().bounds.size)")
+    print("viewDidLoad \(scrollView.frame)")
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    print("will apper \(scrollView.frame)")
+    
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    print("will apper \(scrollView.frame)")
+    
+    scrollView.fixImage(imageView: imageView)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  //: CommnetTest
+  func commentTest() {
+    
+  }
 }
 
