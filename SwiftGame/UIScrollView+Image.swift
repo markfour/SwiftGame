d9f0aa8f5090677eed3245ccbdc6b1734f5792fc
hayashidakazumi <axhfaro@gmail.com>
2015-11-08 18:01:30 +0900

    Add plist data

--- SwiftGame/UIScrollView+Image.swift
+++ SwiftGame/UIScrollView+Image.swift
@@ -9,43 +9,25 @@
 import UIKit
 
 extension UIScrollView {
-    func fixImage(imageView imageView:UIImageView) {
-        // Fit image
-        var resize = frame.size
-        
-        let wRatio = frame.size.width / (imageView.image?.size.width)!
-        let hRatio = frame.size.height / (imageView.image?.size.height)!
-        
-        print(frame)
-        print("wr \(wRatio) hr \(hRatio)")
-        print("image \(imageView.image?.size)")
-        
-        
-        if wRatio < hRatio {
-            // height is big
-            let ratio = resize.height / hRatio
-            resize.width = (imageView.image?.size.width)! * hRatio
-            
-        } else {
-            // width is big
-            let ratio = resize.width / wRatio
-            resize.width = (imageView.image?.size.width)! * hRatio
-        }
-        print("imageview image \(imageView.image?.size)")
-        print(resize)
-        
-//        UIGraphicsBeginImageContext(resize)
-//        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-        
-        imageView.image = imageView.image?.resizeToSize(resize)
-
-        
-        imageView.backgroundColor = UIColor.blueColor()
-        
-//        imageView.frame = CGRectMake(0, 0, (imageView.image?.size.width)!, (imageView.image?.size.height)!)
-        
-        print(imageView.frame)
-        print(imageView.image?.size)
+  func fixImage(imageView imageView:UIImageView) {
+    // Fit image
+    var resize = frame.size
+    
+    let wRatio = frame.size.width / (imageView.image?.size.width)!
+    let hRatio = frame.size.height / (imageView.image?.size.height)!
+    
+    if wRatio < hRatio {
+      // height is big
+      resize.width = (imageView.image?.size.width)! * hRatio
+      
+    } else {
+      // width is big
+      resize.width = (imageView.image?.size.width)! * hRatio
     }
+    
+    imageView.image = imageView.image?.resizeToSize(resize)
+    
+    
+    imageView.backgroundColor = UIColor.blueColor()
+  }
 }
--- SwiftGame/ViewController.swift
+++ SwiftGame/ViewController.swift
@@ -8,31 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var scrollView: UIScrollView!
   @IBOutlet weak var imageView: UIImageView!
   
+  var mainMenu = [Dictionary<String, AnyObject>]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     
-    
-    //        view.frame = CGRectMake(0, 0, CGRectGetWidth(UIScreen.mainScreen().bounds), CGRectGetWidth(UIScreen.mainScreen().bounds))
-    
-    print("window \(UIScreen.mainScreen().bounds.size)")
-    print("viewDidLoad \(scrollView.frame)")
+    let path = NSBundle.mainBundle().pathForResource("BuildingMenu.plist", ofType:nil )
+    mainMenu = NSArray(contentsOfFile: path!) as! Array
+    print(mainMenu.count)
   }
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
     
-    print("will apper \(scrollView.frame)")
-    
   }
   
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-    print("will apper \(scrollView.frame)")
     
     scrollView.fixImage(imageView: imageView)
   }
@@ -42,10 +39,31 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
-  //: CommnetTest
-  func commentTest() {
-    
+  // MARK: -
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
   }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return mainMenu.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = UITableViewCell.init()
+    
+    let menu = mainMenu[indexPath.row]
+    cell.textLabel?.text = menu["name"] as? String
+    
+    return cell
+  }
+  
+//  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//    let cell = tableView.dequeueReusableCellWithIdentifier( "LabelCell", forIndexPath: indexPath)
+//    
+//   ... // Configure the cell
+//    cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+//    
+//    return cell
+//  }
 }
 
