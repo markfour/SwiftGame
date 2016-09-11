dbc9a91bba66131d33e93dab4d1bb849215ff01e
markfour <kazumihayashida@kazumi-no-MacBook-Air.local>
2016-09-11 23:50:45 +0900

    Add BuildInfo

---
+++ SwiftGame/BuildInfo.swift
@@ -0,0 +1,13 @@
+//
+//  BuildInfo.swift
+//  SwiftGame
+//
+//  Created by kazumi hayashida on 2016/09/11.
+//  Copyright © 2016年 HayashidaKazumi. All rights reserved.
+//
+
+import Foundation
+
+class buildInfo {
+    
+}
